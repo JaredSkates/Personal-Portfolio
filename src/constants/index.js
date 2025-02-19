f9ab57e9bd09800f590c312550333baa657c00be
JaredSkates <jaredskatesz12@gmail.com>
2025-02-18 22:25:51 -0800

    Fixed Experiences Content

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,19 +8,13 @@ export const EXPERIENCES = [
         technologies: ["TypeScript", "Node.js", "Vue.js"],
     },
     {
-        date: "Nov 2024 - Dec 2024",
-        role: "SWE Micro-Intern",
-        company: "CodeDay Labs",
-        description: `Collaborated with a team of 4 to implement Version Entities,
-                    a feature that allows data structures to maintain compatability.`,
-        technologies: ["TypeScript", "Node.js", "Vue.js"],
+        date: "Jun 2024 - Jun 2024",
+        role: "Volunteer",
+        company: "Area Youth Ministry",
+        description: `Collaborated with 2 coaches to teach the fundamentals of
+                    basketball to a small cohort of kids. Helped organize and create
+                    a welcoming environment.`,
+        technologies: ["Collaboration", "Node.js", "Vue.js"],
     },
-    {
-        date: "Nov 2024 - Dec 2024",
-        role: "SWE Micro-Intern",
-        company: "CodeDay Labs",
-        description: `Collaborated with a team of 4 to implement Version Entities,
-                    a feature that allows data structures to maintain compatability.`,
-        technologies: ["TypeScript", "Node.js", "Vue.js"],
-    },
+    
 ];
